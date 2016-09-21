cbdd6dae0b08366429171b89116be7070be16ab1
qtotuan <quynh.totuan@gmail.com>
2016-09-21 12:32:50 -0400

    Second commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var newColor;
+var color = $(".selected").css("background-color");
+
+$(".controls li").click(function() {
+  $(this).siblings().removeClass("selected");
+  $(this).addClass("selected");
+  color = $(this).css("background-color");
+});
+
+$("#revealColorSelect").click(function() {
+  $("#colorSelect").toggle();
+});
+
+//Store color that user has made in a variable
+function getNewColor () {
+  var red = $("#red").val();
+  var green = $("#green").val();
+  var blue = $("#blue").val();
+  newColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+  return newColor;
+}
+
+//Update the color select field with th new color the user is making
+function changeColor () {
+  newColor = getNewColor();
+  $("#newColor").css("background-color", newColor);
+}
+
+$("input[type=range]").change(changeColor);
+
+//Add new color to the color controls when user clicks add button
+function addColor () {
+  $("ul").append("<li></li>").css("background", );
+}
+
+//$("#addNewColor").click();
